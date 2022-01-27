73b2d9425d734d717d950897b255dd1b36d526c9
Jason Pickens <jasonpickensnz@gmail.com>
2022-01-27 14:14:24 +1300

    Update setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,15 @@
 
 { # Prevent execution if this script was only partially downloaded.
 
+  set -euo pipefail
+
+  echo "Downloading and running Nix installer"
+
   # Install Nix.
   # See https://nixos.org/download.html#nix-install-macos.
-  #sh <(curl -L https://nixos.org/nix/install)
+  sh <(curl -L https://nixos.org/nix/install)
+
+  echo "Downloading the setup script and running in a sub shell"
 
   # Continue the rest of the install in a new shell.
   sh <(curl -L https://raw.githubusercontent.com/steinybot/bootstrap/main/setup.sh)
--- setup.sh
+++ setup.sh
@@ -4,6 +4,15 @@
 
 { # Prevent execution if this script was only partially downloaded.
 
-  echo hello
+  set -euo pipefail
 
-} # End of wrapping.
+  # Set NIX_PATH explicitly to work around https://github.com/NixOS/nixpkgs/issues/149791.
+  # The path is the same as the default when NIX_PATH is not set (https://github.com/NixOS/nix/blob/master/src/libexpr/eval.cc).
+  NIX_STATE_DIR="/nix/var/nix"
+  export NIX_PATH="${NIX_PATH:-${HOME}/.nix-defexpr/channels:nixpkgs=${NIX_STATE_DIR}/profiles/per-user/root/channels/nixpkgs:${NIX_STATE_DIR}/profiles/per-user/root/channels}"
+
+  nix-shell "https://github.com/nix-community/home-manager/archive/master.tar.gz" -A install
+
+}
+
+# End of wrapping.
