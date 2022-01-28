e57b157c0f015d3e3c897efe7c1744bc5538f249
Jason Pickens <jasonpickensnz@gmail.com>
2022-01-28 14:28:15 +1300

    Remove unnecessary double bootstrap

--- default.nix
+++ default.nix
@@ -1,11 +1,20 @@
 { pkgs ? import <nixpkgs> {} }:
 
+let
+  # The path to the dotfiles repository once it has been checked out.
+  dotFilesRepo = fetchGit {
+    url = "https://github.com/steinybot/dotfiles.git";
+    ref = "main";
+  };
+
+  homeNixFile = "${dotFilesRepo}/home/.config/nixpkgs/home.nix";
+in
 pkgs.mkShell {
   buildInputs = with pkgs; [
     git
   ];
   shellHook = ''
     set -euo pipefail
-    home-manager -f '${./home-bootstrap.nix}' -b 'backup-before-bootstrap' -v switch
+    home-manager -f '${homeNixFile}' -b 'backup-before-bootstrap' -v switch
   '';
 }
