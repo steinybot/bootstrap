9a3723db36303d1e8fb76b7b70949a67137d89a6
Jason Pickens <jasonpickensnz@gmail.com>
2022-02-16 18:11:24 +1300

    Add pinentry

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@
 
   sign_in_to_app_store() {
     echo "Please sign in to the App Store"
-    nix-shell -p lastpass-cli
+    nix-shell -p lastpass-cli -p pinentry_mac
 
     # TODO: Get Apple ID password and read for enter key.
     exit 1
