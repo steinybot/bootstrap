d5387e5afa6c76f48d3fbe382d6a36d943072e3e
Jason Pickens <jasonpickensnz@gmail.com>
2022-03-08 09:48:40 +1300

    Add check for nix

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,18 +7,30 @@
   set -euo pipefail
 
   install_nix() {
-    echo "Downloading and running Nix installer"
+    echo "Downloading and running Nix installer."
 
     sh <(curl -L https://nixos.org/nix/install)
+  
+    echo "Please restart your shell and run this script again to continue."
+    exit 0
+  }
+  
+  check_nix() {
+    if ! command -v nix &> /dev/null; then
+      echo "Could not find nix. You probably need to restart your shell."
+      exit 1
+    fi
   }
 
   # Install Nix.
   # See https://nixos.org/download.html#nix-install-macos.
   [ -e /nix ] || install_nix
+  
+  check_nix
 
-  echo "Downloading the setup script and running in a sub shell"
+  echo "Downloading the running the setup script."
 
-  # Continue the rest of the install in a new shell.
+  # Continue the rest of the setup.
   bash <(curl -L https://raw.githubusercontent.com/steinybot/bootstrap/main/setup.sh)
 
 } # End of wrapping.
